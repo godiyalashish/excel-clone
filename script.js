f4c3ec2d309eb181d3f7782eb6973f761b7f49c9
godiyalashish <ashishgodiyal.333@gmail.com>
2022-07-06 22:01:41 +0530

    util function getRowIdColIdFromElement created

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
-let cellsContent = document.querySelector(".cells-content");
+let topRow = document.querySelector(".top-row");
+let leftCol = document.querySelector(".left-col");
+let topLeftCell = document.querySelector(".top-left-cell");
+let allCells = document.querySelectorAll(".cell");
+let addressInput = document.querySelector("#address");
 
+cellsContentDiv.addEventListener("scroll",function(e){
+    let scrollFromTop = e.target.scrollTop;
+    let scrollFromLeft = e.target.scrollLeft;
+    topRow.style.top = scrollFromTop+"px";
+    leftCol.style.left = scrollFromLeft+"px";
+    topLeftCell.style.top = scrollFromTop+"px";
+    topLeftCell.style.left = scrollFromLeft+"px";
+})
 
-function initCells(){
-    let cell = "";
-    for(let i=0;i<100;i++){
-        cell+="<div class = 'row'>";
-        //column
-        for(let j=0;j<26;j++){
-            cell+="<div class = 'cell'>CELL</div>"
+for(let i=0;i<allCells.length;i++){
+    allCells[i].addEventListener("click",function(e){
+        let rowId = Number(e.target.getAttribute("rowid"));
+        let colId = Number(e.target.getAttribute("colid"));
+        let address = String.fromCharCode(65+colId)+(rowId+1)+"";
+        // console.log(address);
+        addressInput.value = address;
+    })
+
+    allCells[i].addEventListener("blur",function(e){
+        let cellValue = e.target.textContent;
+        let {rowId,colId} = getRowIdColIdFromElement(e.target);
+        let cellObject = db[rowId][colId];
+        if(cellObject.value == cellValue){
+            return;
         }
-        cell+="</div>"
-    }
-    cellsContent.innerHTML = cell;
+        cellObject.value = cellValue;
+        console.log("After UPdate",cellObject);
+    })
 }
 
-initCells();
+function getRowIdColIdFromElement(element){
+    let rowId  = element.getAttribute("rowid");
+    let colId = element.getAttribute("colid");
+    return {
+        rowId,
+        colId
+    }
+}
